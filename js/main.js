5e152518b52a92736584db5f601d55d41b05090c
morettisf <morettisf@gmail.com>
2017-05-03 14:44:43 -0700

    removed ES6, adjusted CSS on larger screens

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ var arrow = document.querySelector('#arrow-top');
 var contactWrap = document.querySelector('#contact-wrap');
 
 // intro animation up front
-setTimeout(() => {
+setTimeout(function() {
   h1.classList.add('h1-motion');
 
-    setTimeout(() => {
+    setTimeout(function() {
       h2.classList.add('h2-motion');
 
-        setTimeout(() => {
+        setTimeout(function() {
           arrow.classList.add('blink');
           contactWrap.classList.add('contact-wrap-fade-in');
 
@@ -24,11 +24,11 @@ setTimeout(() => {
 // letter rotation on mouse over
 var letters = document.querySelectorAll('.letter');
 
-letters.forEach((letter) => {
-  letter.addEventListener('mouseenter', (event) => {
+letters.forEach(function(letter) {
+  letter.addEventListener('mouseenter', function(event) {
     letter.classList.add('flip-letter');
 
-    setTimeout(() => {
+    setTimeout(function() {
       letter.classList.remove('flip-letter');
     }, 600)
   })
